a3b7a0567f28eed32b972011bea3a7103a350595
公司 <3113149292@qq.com>
2020-08-21 17:48:48 +0800

    第二天

--- src/components/Index/search.vue
+++ src/components/Index/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search">
-    <div>
-      <input type="text" placeholder="请输入关键字" v-model="searchVal" />
+    <div class="send">
+      <input id="searchInput" type="text" placeholder="请输入关键字" v-model="searchVal" />
       <button class="btn" @click="search">搜索</button>
     </div>
     <div class="searchContent">
@@ -10,6 +10,7 @@
         <i @click="clearVal($event)">X</i>
       </div>
     </div>
+    <div>111s</div>
   </div>
 </template>
 
@@ -32,24 +33,33 @@ export default {
   computed: {},
   // 自定义方法
   methods: {
+    // 点击搜索后，判断历史记录是否有相同的值，有则替换 无则添加
     search: function() {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      const that = this;
       console.log(this.searchVal);
+      // 通过过滤器选中元素并删除
+      this.searchVals.filter(function(item, index) {
+        if (item.val == that.searchVal) {
+          that.searchVals.splice(index, 1);
+          return;
+        }
+      });
       this.searchVals.unshift({ val: this.searchVal });
     },
+    // 点击历史记录 添加到搜索输入框
     getVal(e) {
-      // eslint-disable-next-line @typescript-eslint/no-this-alias
-      // const that = this;
-      // this.searchVal = this.searchVal + "sss";
-      // const clearValContent = e.currentTarget.previousElementSibling.innerHTML; // 待删除的内容
-      console.log(e.target.innerHTML);
+      // console.log(e.target.innerHTML);
       this.searchVal = this.searchVal + e.target.innerHTML;
+      document.getElementById("searchInput").focus(); // input框获取焦点
     },
+    // 清除选中的历史记录
     clearVal(e) {
       // eslint-disable-next-line @typescript-eslint/no-this-alias
       const that = this;
-      // this.searchVal = this.searchVal + "sss";
       const clearValContent = e.currentTarget.previousElementSibling.innerHTML; // 待删除的内容
       // console.log(clearValContent);
+      // 通过过滤器选中元素并删除
       this.searchVals.filter(function(item, index) {
         if (item.val == clearValContent) {
           that.searchVals.splice(index, 1);
@@ -77,4 +87,45 @@ export default {
   // destroyed() {}
 };
 </script>
-<style lang="less" scoped></style>
+<style lang="scss" scoped>
+.search {
+  .send {
+    input {
+      padding: 0 10px;
+    }
+  }
+
+  .searchContent {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    div {
+      display: inline-block;
+      font-size: 6px;
+      font-weight: 700;
+      color: rgb(139, 133, 133);
+      margin-right: 20px;
+      padding: 0 4px;
+      border: 1px solid rgb(218, 214, 214);
+      border-radius: 8px;
+      span {
+        margin-right: 10px;
+        overflow: hidden;
+        display: inline-block;
+        max-width: 100px;
+        max-height: 16px;
+        text-overflow: ellipsis;
+        display: -webkit-inline-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 1;
+        border-right: 1px solid rgb(218, 214, 214);
+      }
+      span:active {
+        background-color: aqua;
+      }
+    }
+  }
+}
+</style>
